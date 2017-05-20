9e4c3d44505c232f3284289f9f0ccf0c186c3d6d
nfgallego <nicolas.fernandez.gallego@gmail.com>
2017-05-20 19:29:57 +0200

    Fourth Commit

--- src/task.java
+++ src/task.java
@@ -21,5 +21,30 @@ public class task
             }
             System.out.println();
         }
+        
+        //ordenar
+        for (int i=0;i<6;i++){
+            for(int j=0;j<2;j++){
+                for (int x=0;x<6;x++){
+                    for(int y=0;y<2;y++){
+                        if(table[i][j] < table[x][y]){
+                            int t = table[i][j];
+                            table [i][j] = table [x][y];
+                            table[x][y] = t;
+                        }
+                    }
+                }
+            }
+        }
+        
+        System.out.print("-----------"+"\n");
+        for (int i=0;i<6;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                System.out.print(table[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
