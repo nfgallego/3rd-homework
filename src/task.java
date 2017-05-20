cdeeb1c7b29d2409d99fdb2ad5ea026bd9aa58d1
nfgallego <nicolas.fernandez.gallego@gmail.com>
2017-05-20 19:27:38 +0200

    Third Commit

--- src/task.java
+++ src/task.java
@@ -5,18 +5,13 @@ public class task
         int table [][];
         table = new int [6][2];
         
-        table[0][0] = 1;
-        table[0][1] = 2;
-        table[1][0] = 3;
-        table[1][1] = 4;
-        table[2][0] = 5;
-        table[2][1] = 6;
-        table[3][0] = 8;
-        table[3][1] = 9;
-        table[4][0] = 10;
-        table[4][1] = 11;
-        table[5][0] = 12;
-        table[5][1] = 13;
+        for (int i=0;i<6;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                table [i][j] = (int) (Math.random()*50);
+            }
+        }
         
         for (int i=0;i<6;i++)
         {
